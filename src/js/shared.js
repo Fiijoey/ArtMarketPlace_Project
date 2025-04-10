e16281d2f06e2bd08c531c1aaa7110eca9c9c926
Fiijoey <kofimensah3730@gmail.com>
2025-04-10 06:39:44 +0000

    Remove unused HTML files and JavaScript assets to streamline the project structure

---
+++ src/js/shared.js
@@ -0,0 +1,23 @@
+import { getLocalStorage } from "./utilities.mjs";
+
+export function updateCartCount(loaded) {
+  if (loaded) {
+    const cartItems = getLocalStorage("so-cart") || [];
+    const cartCountElement = document.querySelector(".cart-count");
+
+    if (cartCountElement) {
+      let count = 0;
+
+      cartItems.forEach((item) => {
+        count += item.quantity;
+      });
+
+      if (count > 0) {
+        cartCountElement.textContent = count; // Update the number
+        cartCountElement.style.display = "block"; // Show the cart count
+      } else {
+        cartCountElement.style.display = "none"; // Hide the cart count if the cart is empty
+      }
+    }
+  }
+}
